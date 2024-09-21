bff6f7c20f0fd0f271fb474322d704e168c28d31
anapaulatf <aptavaresdafonseca@gmail.com>
2024-09-21 12:21:10 -0300

    corrige erro

--- ep02/ep02.py
+++ ep02/ep02.py
@@ -2,10 +2,10 @@ import numpy as np
 from sympy import *
 import matplotlib.pyplot as plt
 
-def diff_y(t_val, y_val):
+def diff_y(t, y):
     '''Retorna a derivada da função fornecida no ponto (t, y)'''
-    f = lambdify([t, y_var], fuser)  # Função f(t, y) a ser aproximada
-    return f(t_val, y_val)
+    f = lambdify([x, y_var], fuser)  # Função f(t, y) a ser aproximada
+    return f(t, y)
 
 # passo
 def step(t0, tf, n):
@@ -16,16 +16,16 @@ def aprox(a, b, n, y0):
     '''Aproximação pelo método de Euler explícito'''
     passo = step(a, b, n)
     # valores para t
-    t_vals = np.arange(a, b+passo, passo)
+    t = np.arange(a, b+passo, passo)
     # valores da função y(t)
-    y_values = np.zeros(len(t_vals))
+    y_values = np.zeros(len(t))
     y_values[0] = y0  # Condição inicial
     
     # Calcula os valores aproximados usando o método de Euler
-    for i in range(len(t_vals)-1):
-        y_values[i+1] = y_values[i] + passo * diff_y(t_vals[i], y_values[i])
+    for i in range(len(t)-1):
+        y_values[i+1] = y_values[i] + passo * diff_y(t[i], y_values[i])
     
-    return t_vals, y_values
+    return t, y_values
 
 def main(a, b):
     '''Compila os valores da aproximação para diferentes valores de n'''
@@ -35,8 +35,8 @@ def main(a, b):
     y0 = float(input('Insira o valor inicial de y(0): '))  # Valor inicial de y(0)
     
     for n in listn:
-        t_vals, y_values = aprox(a, b, n, y0)
-        T.append(t_vals)
+        t, y_values = aprox(a, b, n, y0)
+        T.append(t)
         Y.append(y_values)
 
     # plotar aproximação
@@ -56,7 +56,7 @@ def main(a, b):
     plt.show()
 
 if __name__ == "__main__":
-    t, y_var = symbols('t y')  # Declara as variáveis simbólicas t e y
+    x, y_var = symbols('x y')  # Declara as variáveis simbólicas t e y
     user_input = input("Insira uma equação diferencial f(t, y): ")
     fuser = sympify(user_input)
     a = float(input('Intervalo de integração - valor mínimo (a): '))
